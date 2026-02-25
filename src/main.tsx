351eb21887f643e912a84c7a2d2544316d9e2301
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-02-25 18:36:04 -0300

    chore: use shared styles and align config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '@gaqno-development/frontcore/styles/globals.css'
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
